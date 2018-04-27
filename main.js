86c83982d82e61df3e8c39b8dbdf8f7505d4c8ba
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-04-27 18:59:43 +0300

    add node modules

--- main.js
+++ main.js
@@ -6,20 +6,26 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 
-
+//initialize window
 let win;
 
 function createWindow() {
+    //create browser window
     win = new BrowserWindow();
+
+    //load html page
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
         slashes: true
     }));
+
+    //open devtools only for development mode only
     win.webContents.openDevTools();
     win.on('closed', () => {
         win = null;
     })
 }
 
+//run create window
 app.on('ready', createWindow);
